59aa2d559f73dc92fcfdd4a0c95669bf6984e3d9
Blairis123 <64935808+Blairis123@users.noreply.github.com>
2021-10-25 22:29:48 -0700

    Add files via upload

---
+++ Lab0/Project_0.X/build/default/debug/_ext/1472/lab0.o.d
@@ -0,0 +1,4 @@
+build/default/debug/_ext/1472/lab0.o:  \
+../lab0.c  \
+../CerebotMX7cK.h  \
+ ../lab0.h 
---
+++ Lab0/Project_0.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Project_0.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Project_0.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=project0.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/project0.x.tar
